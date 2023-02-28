5319ebb34b8f41261a383f503360ff18709ec77d
StupidFraid <alexey4he@yandex.ru>
2023-02-28 12:39:16 +0500

    Fix From Windows OS

--- src/Lab_8/Template_0/Primer1.java
+++ src/Lab_8/Template_0/Primer1.java
@@ -5,7 +5,7 @@ import java.io.File;
 public class Primer1 {
     public static void main(String[] args) {
         try {
-            // создаиние файла в текущей папке ( где расположен файл lassFile1.java)
+            // создаиние файла в текущей папке ( где расположен файл Primer1.java)
             File f1 = new File("MyFile.txt");
             f1.createNewFile();
             if(f1.exists()){
@@ -14,12 +14,12 @@ public class Primer1 {
             }
 
             // Создание файла в домашней директории пользователя, и вывод полного пути
-            File f2 = new File("/Users/alexey4he/Documents/MyFile2.txt");
+            File f2 = new File("/Lab_8_Dir_My/MyFile2.txt");
             f2.createNewFile();
             System.out.println("Полный путь 2: " + f2.getAbsolutePath());
 
             // Создание нескольки вложенны папок
-            File f3 = new File("/Users/alexey4he/Documents/Learning/Programing/Java");
+            File f3 = new File("/Lab_8_Dir_My/Java_Test/testMkdirs");
             f3.mkdirs();
             System.out.println("Полный путь 3: " + f3.getAbsolutePath());
         } catch (Exception e){
@@ -27,3 +27,6 @@ public class Primer1 {
         }
     }
 }
+
+//Создание файлов и папок
+//!!! Команды где создается файл, необходимо обезательно помешать в блок try-catch
