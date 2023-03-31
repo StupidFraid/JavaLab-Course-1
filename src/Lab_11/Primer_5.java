0ccc3c6a8a161ca69d27eed800190908b1933ad4
StupidFraid <alexey4he@yandex.ru>
2023-03-31 18:50:33 +0500

    Progress in Lab 11

---
+++ src/Lab_11/Primer_5.java
@@ -0,0 +1,14 @@
+package Lab_11;
+
+public class Primer_5 {
+    public static void main(String[] args) {
+        System.out.println(fact(8));
+    }
+    public static int fact(int n){
+        if (n == 0) return 0;
+        else if (n==1) return 1;
+        else {
+            return fact(n - 2) + fact(n - 1);
+        }
+    }
+}
