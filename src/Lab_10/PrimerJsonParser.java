304901aa1a040825fee5a3c5a996efbb648f4127
StupidFraid <alexey4he@yandex.ru>
2023-03-31 17:12:00 +0500

    Complete Lab 10

---
+++ src/Lab_10/PrimerJsonParser.java
@@ -0,0 +1,30 @@
+package Lab_10;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.io.FileReader;
+
+public class PrimerJsonParser {
+    public static void main(String[] args) {
+        try {
+            JSONParser parser = new JSONParser();
+            Object obj = parser.parse(new FileReader
+                    ("Programming_Git/src/Lab_10/example_files/exampleJsonToJsonParser.json"));
+            JSONObject jsonObject = (JSONObject) obj;
+            System.out.println("Корневой элемент: " + jsonObject.keySet().iterator().next());
+            JSONArray jsonArray = (JSONArray) jsonObject.get("books");
+
+            for (Object o : jsonArray){
+                JSONObject book = (JSONObject) o;
+                System.out.println("\nТекущий элемент: book");
+                System.out.println("Название книги: " + book.get("title"));
+                System.out.println("Автор книги: " + book.get("author"));
+                System.out.println("Название книги: " + book.get("year"));
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/Lab_10/Task2/Main.java
@@ -0,0 +1,43 @@
+package Lab_10.Task2;
+
+//import Lab_10.Task2.ParserJson;
+
+import Lab_10.Task1.ParserXml;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        ParserJson ParserJson = new ParserJson();
+        System.out.println();
+        System.out.println("Основное меню (Парсер JSON): ");
+        System.out.println("Нажмите '1' что бы вывести список книг");
+        System.out.println("Нажмите '2' что бы добавить новую книгу");
+        System.out.println("Нажмите '3' что бы найти книгу по названию или автору");
+        System.out.println("Нажмите '4' что бы удалить книгу по названию");
+        System.out.println("Нажмите '5' что бы закрыть программу");
+        System.out.print("Выбранно: ");
+
+        int choiceOption = in.nextInt();
+        in.nextLine();
+
+        switch (choiceOption){
+            case 1:
+                ParserXml.printBook(ParserJson.parser(), "JSON");
+                break;
+            case 2:
+                ParserJson.addNewBook();
+                break;
+            case 3:
+                ParserXml.printBook(ParserXml.findBook(ParserJson.parser()), "JSON c указанным автором");
+                break;
+            case 4:
+                ParserJson.deleteBook();
+                break;
+            case 5:
+                System.exit(0);
+                break;
+        }
+    }
+}
