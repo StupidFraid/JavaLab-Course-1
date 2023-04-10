b5a4204d0fa2ea0cec9bb47a3ce178cf1fbfe0b3
StupidFraid <alexey4he@yandex.ru>
2023-04-10 17:42:17 +0500

    Progress lab - 11

--- src/Lab_11/Task7/Main.java
+++ src/Lab_11/Task7/Main.java
@@ -4,21 +4,30 @@
 //Одна из программ должна использовать класс ArrayList, а вторая — LinkedList
 package Lab_11.Task7;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.println("Введите количество людей в кругу");
-        int numberPeople = in.nextInt();
+//        Scanner in = new Scanner(System.in);
+//        System.out.println("Введите количество людей в кругу");
+        int numberPeople = 100000;
 
-        arraysListCounting(numberPeople);
+        System.out.println("Посчитаем время выполнения, ArrayList с " +  numberPeople + " человек");
+        long arrayListTimeFinish = arraysListCounting(numberPeople);
+        System.out.println("Время выполнения " + arrayListTimeFinish + " милисекунд");
+        long linkedListTimeFinish = LinkedListCounting(numberPeople);
+        System.out.println("Время выполнения " + linkedListTimeFinish + " милисекунд");
+
     }
 
-    public static void arraysListCounting(int numberPeople){
+    public static long arraysListCounting(int numberPeople){
         ArrayList<People> peopleArrayList= new ArrayList<People>();
         People people = null;
+        System.out.println("Игра в ArrayList началась...");
+        long startTime = System.currentTimeMillis();
 
         for (int i = 0; i < numberPeople; i++){
             people = new People(i);
@@ -31,9 +40,39 @@ public class Main {
                     peopleArrayList.remove(i);
                 }
             }
-            System.out.println("Длина списка после выбывания равна " + peopleArrayList.size());
+//            System.out.println("Длина списка после выбывания равна " + peopleArrayList.size());
+
         }
+        long endTime = System.currentTimeMillis();
+        String lastPeople = String.valueOf(peopleArrayList.get(0).number);
+        System.out.println("В кругу ArrayList остался человек с номером: " + lastPeople);
+        return endTime - startTime;
     }
 
+    public static long LinkedListCounting(int numberPeople) {
+        LinkedList<People> peopleLinkedList = new LinkedList<People>();
+        People people = null;
+        System.out.println("Игра в LinkedList началась...");
+        long startTime = System.currentTimeMillis();
 
+        for (int i = 0; i < numberPeople; i++) {
+            people = new People(i);
+            peopleLinkedList.add(people);
+        }
+
+        while (peopleLinkedList.size() > 1) {
+            for (int i = 0; i < peopleLinkedList.size(); i++) {
+                if (i % 2 == 0) {
+                    peopleLinkedList.remove(i);
+                }
+            }
+//            System.out.println("Длина списка после выбывания равна " + peopleLinkedList.size());
+
+        }
+        String lastPeople = String.valueOf(peopleLinkedList.get(0).number);
+        System.out.println("В кругу LinkedList остался человек с номером: " + lastPeople);
+
+        long endTime = System.currentTimeMillis();
+        return endTime - startTime;
+    }
 }
